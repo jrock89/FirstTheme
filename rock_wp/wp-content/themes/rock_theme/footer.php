557a574a2d24d367b89aa43c104a0aadb96258a9
Jesse <Jesserock11@gmail.com>
2015-11-26 01:27:08 -0500

    added

---
+++ rock_wp/wp-content/themes/rock_theme/footer.php
@@ -0,0 +1,57 @@
+<!-- FOOTER SECTION -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-3 text-center margin-bottom">
+        <h2>Keep in touch</h2>
+        <img src="images/divider-silver.png" alt="divider" />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-4">
+        <address class="">
+          Rock Theme<br>
+          501 Union Ave<br>
+          Saratoga Springs, Ny 12866
+        </address>
+      </div>
+      <div class="col-sm-4">
+        <ul>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="">Latest News</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-4">
+        <p>
+          Follow Us
+        </p>
+        <i class="fa fa-facebook fa-lg"></i>
+        <i class="fa fa-twitter fa-lg"></i>
+        <i class="fa fa-google-plus fa-lg"></i>
+        <i class="fa fa-pinterest fa-lg"></i>
+        <i class="fa fa-youtube fa-lg"></i>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="container-fluid" id="copy">
+    <div class="col-sm-12">
+      <p>
+        &copy; 2015 Rock Theme
+      </p>
+    </div>
+  </div>
+
+</footer>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ rock_wp/wp-content/themes/rock_theme/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Rock</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- custome css -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- custom google font -->
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,700,300' rel='stylesheet' type='text/css'>
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
+    <!-- HEADER  -->
+    <header>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" name="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle Navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a href="index.html" class="navbar-brand">Rock Theme</a>
+          </div>
+          <!-- navbar header end -->
+
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="">Latest News</a></li>
+            </ul>
+          </div>
+          <!-- navrbar collapse end -->
+        </div>
+        <!-- container fluid end -->
+      </nav>
+
+      <!-- hero section -->
+      <div class="container-fluid">
+        <div class="row feature">
+          <img src="images/lakebg.jpg" alt="lake background" />
+
+          <div class="feature-text col-xs-12 col-md-8 col-lg-6 col-md-offset-2 col-lg-offset-3">
+            <p>
+              Fully Responsive<br>Premium theme<br>for wordpress.
+            </p>
+          </div>
+          <!-- feature text end -->
+        </div>
+        <!-- row end -->
+      </div>
+      <!-- fluid container end -->
+
+    </header>
